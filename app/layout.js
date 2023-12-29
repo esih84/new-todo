d563267565bccf45576bb544ef66684a59fe3013
unknown <esmaeilh.haji84@gmail.com>
2023-12-29 22:42:35 +0330

    feat(layout): install next-auth add provider
    install nexauth and add sessionProvider to layout

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import ReactQueryProvide from '@/providers/reactQueryProvide'
+import NextAuthProvider from '@/providers/nextAuthProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +15,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <ReactQueryProvide>
-          {children}
+          <NextAuthProvider>
+            {children}
+          </NextAuthProvider>
         </ReactQueryProvide>  
         
       </body>
