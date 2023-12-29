5fe3dea76e8e6dcc56e628f87e8b5a565558dce1
unknown <esmaeilh.haji84@gmail.com>
2023-12-29 22:47:05 +0330

    feat(SingIn): create signIn page and api

---
+++ app/(authentication)/SignIn/page.js
@@ -0,0 +1,12 @@
+'use client'
+
+import SignInPage from "@/components/authentication/SignIn";
+
+const SignIn = () => {
+
+    return (
+        <SignInPage/>
+    );
+}
+
+export default SignIn;
---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,50 @@
+import { compare } from "bcryptjs";
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials";
+
+
+export const authOptions = {
+
+  providers: [
+  
+          CredentialsProvider({
+            name: "Credentials",
+
+            async authorize(credentials) {
+              try {
+                  // console.log(credentials)
+                  const {email, password} = credentials
+                  // console.log(email,password)
+                  if (!email || !password) {
+                    throw new Error("لطفا اطلاعات معتبر وارد کنید");
+                  }
+                  
+                  const res = await fetch(`http://localhost:4000/users?email=${email}`)
+                  const user = await res.json()
+                  // console.log(user[0] )
+
+                  if (!user || !user[0]?.hashedPassword) {
+                    throw new Error("لطفا ابتدا حساب کاربری ایجاد کنید");
+                  }
+                  
+                  const isValid = await compare(
+                    password,
+                    user[0].hashedPassword
+                  );
+                  // console.log(isValid)
+                  if (!isValid) {
+                    throw new Error("ایمیل یا رمز عبور اشتباه است");
+                  }
+                  return {email};
+              } catch (error) {
+                // console.log(error)
+                throw new Error(error.message);
+                
+              }
+            },
+          })
+  ],
+}
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
