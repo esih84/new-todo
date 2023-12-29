8212c95a9bb52ad563b183485224a62a328223ba
unknown <esmaeilh.haji84@gmail.com>
2023-12-29 23:13:36 +0330

    feat(header): create header for site
    create layout for (root)
    and add header

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import ReactQueryProvide from '@/providers/reactQueryProvide'
 import NextAuthProvider from '@/providers/nextAuthProvider'
+import Header from '@/components/layouts/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <ReactQueryProvide>
           <NextAuthProvider>
+            <Header/>
             {children}
           </NextAuthProvider>
         </ReactQueryProvide>  
