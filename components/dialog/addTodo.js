eaf9d076be7ae84305d8b4f8e4065b8829b4c3b5
unknown <esmaeilh.haji84@gmail.com>
2023-12-30 12:22:20 +0330

    fix(addTodo): add revalidation of todos after create todo

--- components/dialog/addTodo.js
+++ components/dialog/addTodo.js
@@ -6,26 +6,28 @@ import toast from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
 import DialogBase from './dailog';
 import { addTodo } from '@/server/action';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export default function AddTodo() {
+  const queryClient = useQueryClient()
     const router = useRouter()
     const [todo, setTodo]= useState("")
     const addModal =  useTodo()
     // console.log(todo)
-    const mutation = useMutation({mutationFn:addTodo})
+    const mutation = useMutation({mutationFn:addTodo,
+      onSuccess:()=>{
+      toast.success("یادداشت ایجاد شد")
+      setTimeout(()=>addModal.onClose(),1000)
+      setTodo("")
+      queryClient.invalidateQueries({queryKey:['todos']})
+    }})
     const submitHandler =async(e)=>{
         e.preventDefault()
 
         mutation.mutate(todo)
         console.log(mutation)
-          if (mutation.isSuccess) {
-            toast.success("یادداشت ایجاد شد")
-            setTimeout(()=>addModal.onClose(),1000)
-            setTodo("")
-            router.refresh()
-          }
-          if(mutation.error){
+
+        if(mutation.error){
             toast.error("مشکلی پیش آمده")
           }
 
